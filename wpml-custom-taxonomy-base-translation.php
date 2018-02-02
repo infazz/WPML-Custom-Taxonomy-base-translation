4ec25f91ed943351264a0586928343510284abd3
Gleb Makarov <gleb@blueglass.ee>
2018-02-02 11:39:32 +0200

    Description update

--- wpml-custom-taxonomy-base-translation.php
+++ wpml-custom-taxonomy-base-translation.php
@@ -15,9 +15,16 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
+ * 
  *
+ *
+ 
+    USAGE:
+    Include this file to your function.php
 
- Add custom taxonomy as you would usually do, and add static slug
+    AND
+
+    Add custom taxonomy as you would usually do, if you dont have one yet.
 
     function add_post_extra() {
         $labels = array(
